c4f886beecd45be46e3aa223ce481472a845908b
dottbadano <dott.badano@gmail.com>
2026-08-30 19:37:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ if "db_pazienti" not in st.session_state:
     from utils import carica_db_pazienti
     st.session_state["db_pazienti"] = carica_db_pazienti()
 
-# Menu di navigazione
-st.sidebar.title("🩺 DSS Urologia")
+# Menu di navigazione con icona vescica aggiornata
+st.sidebar.title("🫀 DSS Urologia") # oppure "💧 DSS Urologia"
 scelta_modulo = st.sidebar.radio(
     "Seleziona Modulo Organo:",
-    ["🧬 Prostata", "🩺 Vescica", "🫘 Rene", "⚾ Testicolo"]
+    ["🧬 Prostata", "💧 Vescica", "🫘 Rene", "⚾ Testicolo"]
 )
 
-# Caricamento moduli
+# Caricamento moduli dinamico
 if scelta_modulo == "🧬 Prostata":
     import prostate
     prostate.render_modulo()
-elif scelta_modulo == "🩺 Vescica":
-    st.info("Modulo Vescica in fase di creazione...")
+elif scelta_modulo == "💧 Vescica":
+    import vescica
+    vescica.render_modulo()
 elif scelta_modulo == "🫘 Rene":
     st.info("Modulo Rene in fase di creazione...")
 elif scelta_modulo == "⚾ Testicolo":
